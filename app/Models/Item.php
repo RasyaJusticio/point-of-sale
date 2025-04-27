3c22d751709bf01e4afa1f76de32b84a71d8f0a4
RasyaJusticio <rasyajusticio.personal@gmail.com>
2025-04-27 16:33:09 +0700

    feat(item): create Item model and migration for items table

---
+++ app/Models/Item.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    public $timestamps = false;
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'stock',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
