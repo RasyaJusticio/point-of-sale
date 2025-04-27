76c8a8938cac3c961789c98e90bba4b210ccfe58
RasyaJusticio <rasyajusticio.personal@gmail.com>
2025-04-27 15:59:49 +0700

    feat(toast): integrate Sonner for flash notifications in app layout

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,14 +1,39 @@
 import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { type BreadcrumbItem } from '@/types';
-import { type ReactNode } from 'react';
+import { usePage } from '@inertiajs/react';
+import { CheckCircle, XCircle } from 'lucide-react';
+import { useEffect, type ReactNode } from 'react';
+import { toast, Toaster } from 'sonner';
 
 interface AppLayoutProps {
     children: ReactNode;
     breadcrumbs?: BreadcrumbItem[];
 }
 
-export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
-    <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
-        {children}
-    </AppLayoutTemplate>
-);
+export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => {
+    const pageProps = usePage().props; // Inertia automatically includes the flash data here
+
+    useEffect(() => {
+        if (pageProps.flash) {
+            const flash: { success?: string; error?: string } = pageProps.flash;
+
+            if (flash.success)
+                toast('Berhasil', {
+                    description: flash.success,
+                    icon: <CheckCircle color="green" size={16} />,
+                });
+            if (flash.error)
+                toast('Gagal', {
+                    description: flash.error,
+                    icon: <XCircle color="red" size={16} />,
+                });
+        }
+    }, [pageProps]);
+
+    return (
+        <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
+            {children}
+            <Toaster />
+        </AppLayoutTemplate>
+    );
+};
