60a30a541afe3f9341f87312a2be2b621c408be1
RasyaJusticio <rasyajusticio.personal@gmail.com>
2025-04-27 13:55:03 +0700

    feat(datatable): enhance DataTable props for improved functionality

--- resources/js/components/ui/datatable.tsx
+++ resources/js/components/ui/datatable.tsx
@@ -11,8 +11,9 @@ import { Button } from './button';
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
     data: PaginationData<TData>;
-    onSearchRoute: string;
     filters: FiltersData;
+    onStoreRoute: string;
+    onSearchRoute: string;
 }
 
 export type FiltersData = {
@@ -23,7 +24,7 @@ export type FiltersData = {
     };
 };
 
-export function DataTable<TData, TValue>({ columns, data, onSearchRoute, filters }: DataTableProps<TData, TValue>) {
+export function DataTable<TData, TValue>({ columns, data, onSearchRoute, onStoreRoute, filters }: DataTableProps<TData, TValue>) {
     const [search, setSearch] = useState(filters.search || '');
 
     const table = useReactTable({
@@ -50,32 +51,38 @@ export function DataTable<TData, TValue>({ columns, data, onSearchRoute, filters
 
     return (
         <div>
-            <form onSubmit={handleSearch} className="mb-4 w-64">
-                <label
-                    className={cn(
-                        'border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 items-center gap-2 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow,border] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-                        'focus-within:border-ring focus-within:ring-ring/50 focus-within:ring-[3px]',
-                        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-                    )}
-                >
-                    <Search size={16} />
-                    <input
-                        type="text"
-                        className="flex-1 bg-transparent outline-none"
-                        placeholder="Cari kategori..."
-                        value={search}
-                        onChange={(e) => setSearch(e.target.value)}
-                    />
+            <div className="flex w-full items-center justify-between">
+                <form onSubmit={handleSearch} className="mb-4 w-64">
+                    <label
+                        className={cn(
+                            'border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 items-center gap-2 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow,border] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+                            'focus-within:border-ring focus-within:ring-ring/50 focus-within:ring-[3px]',
+                            'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+                        )}
+                    >
+                        <Search size={16} />
+                        <input
+                            type="text"
+                            className="flex-1 bg-transparent outline-none"
+                            placeholder="Cari kategori..."
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
 
-                    {search.trim() !== '' && (
-                        <button type="button" onClick={onClearSearch}>
-                            <X size={14} />
-                        </button>
-                    )}
-                </label>
-            </form>
+                        {search.trim() !== '' && (
+                            <button type="button" onClick={onClearSearch}>
+                                <X size={14} />
+                            </button>
+                        )}
+                    </label>
+                </form>
 
-            <div className="roounded-md rounded-md border px-3 py-2 shadow-sm">
+                <Link href={onStoreRoute}>
+                    <Button>Buat Kategori</Button>
+                </Link>
+            </div>
+
+            <div className="rounded-md border px-3 py-2 shadow-sm">
                 <Table>
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (
